9510a8106a4c7c085dc69af9c1121c9303fab0b3
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-11-11 17:50:30 -0300

    hr_payroll

---
+++ hr_payroll/src/main/java/com/jhonatanfrade/hrpayroll/HrPayrollApplication.java
@@ -0,0 +1,17 @@
+package com.jhonatanfrade.hrpayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@RibbonClient(name="rh-worker")
+@EnableFeignClients
+@SpringBootApplication
+public class HrPayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayrollApplication.class, args);
+	}
+
+}
